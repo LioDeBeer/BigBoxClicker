5b7759065e751054e8da4c98df31196c21327459
LioDeBeer <47829338+LioDeBeer@users.noreply.github.com>
2020-04-06 11:11:56 -0700

    Initial push for clicker

---
+++ Assets/Scripts/Client/CookieUI.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CookieUI : MonoBehaviour
+{
+    public Text cookieCountText;
+
+    private Task<int> countTask = null;
+    //don't want a server message on each click
+    private int confirmedCookies = 0;
+    private int bufferedClicks = 0;
+    private int predictedClicks = 0;
+    private float lastSendTime = 0;
+
+    private static float MinSendTime = 0.1f;
+    private static string userId = "TestUser";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CookieServer.Instance.BeginSession(userId);
+        cookieCountText.text = "";
+        countTask = CookieServer.Instance.GetCookieCount(userId);
+    }
+
+    private void OnDestroy()
+    {
+        CookieServer.Instance.EndSession(userId);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countTask != null && countTask.IsCompleted)
+        {
+            confirmedCookies = countTask.Result;
+            cookieCountText.text = (confirmedCookies + bufferedClicks).ToString();
+            countTask = null;
+            predictedClicks = 0;
+        }
+    }
+
+    public void OnCookieClicked()
+    {
+        ++bufferedClicks;
+        ++predictedClicks;
+        cookieCountText.text = (predictedClicks + confirmedCookies).ToString();
+        float elapsedTime = Time.time - lastSendTime;
+        if (elapsedTime >= MinSendTime && countTask == null)
+        {
+            countTask = CookieServer.Instance.ClickCookie(userId, bufferedClicks);
+            bufferedClicks = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Server/CookieServer.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+using UnityEngine;
+
+public class UserData
+{
+    public int cookieCount;
+}
+
+public class CookieServer
+{
+    static CookieServer smInst = new CookieServer();
+    public static CookieServer Instance
+    {
+        get
+        {
+            return smInst;
+        }
+    }
+
+
+    Dictionary<string, UserData> userData = new Dictionary<string, UserData>();
+
+    public void BeginSession(string userId)
+    {
+        string path = Application.persistentDataPath + userId + ".json";
+        UserData user = new UserData();
+        if (File.Exists(path))
+        {
+            string jsonData = File.ReadAllText(path);
+            user = JsonUtility.FromJson<UserData>(jsonData);
+        }
+        userData[userId] = user;
+    }
+
+    public void EndSession(string userId)
+    {
+        UserData user;
+        if (userData.TryGetValue(userId, out user))
+        {
+            string path = Application.persistentDataPath + userId + ".json";
+            File.WriteAllText(path, JsonUtility.ToJson(user));
+        }
+    }
+
+    public async Task<int> ClickCookie(string userId, int clickCount)
+    {
+        await Task.Delay(16);
+        UserData user;
+        if (userData.TryGetValue(userId, out user))
+        {
+            user.cookieCount += clickCount;
+            return user.cookieCount;
+        }
+        return 0;
+    }
+
+    public async Task<int> GetCookieCount(string userId)
+    {
+        await Task.Delay(16);
+        UserData user;
+        if (userData.TryGetValue(userId, out user))
+        {
+            return user.cookieCount;
+        }
+        return 0;
+    }
+
+
+}
